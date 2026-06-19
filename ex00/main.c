72f370fcfc67221af85a74ae735263dc1770c47f
maiguna <maiguna@f1r4s22.paris.42.school>
2026-06-19 11:21:34 +0200

    feat(parsing) : add function check_arg

--- ex00/main.c
+++ ex00/main.c
@@ -1,9 +1,41 @@
 // add header
 
 #include "main.h"
+#include <stdio.h> // a supp
 
-int	main()
+int	check_arg(char *str)
 {
-	write(1, "hello\n", 6);
-	return (0);
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (i % 2 == 0)
+		{
+			if (str[i] < '1' || str[i] > '4')
+				return (0);
+		}
+		else
+		{
+			if (!(str[i] == ' '))
+				return (0);
+		}
+		i++;
+	}
+	if (i != 31)
+		return (0);
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	// t_data	data;
+
+	if (ac != 2 || !check_arg(av[1]))
+	{
+		write(2, "Error\n", 6);
+		return (1);
+	}
+
+	return	(0);
 }
--- ex00/main.h
+++ ex00/main.h
@@ -1,3 +1,9 @@
 // add header
 
 #include <unistd.h>
+
+typedef struct data
+{
+	int	grid[4][4];
+	int	nbrs[16];
+}	t_data;
